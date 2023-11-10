e08c0f4e7eec0966761ccf87b6f5a37cd9ac0a0a
isatkm21 <isabellamanjarres67@gmail.com>
2023-11-09 22:59:11 -0500

    tercero

--- BLL/ResultadoEleccionService.cs
+++ BLL/ResultadoEleccionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ENTITY;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class ResultadoEleccionService
+    public class ResultadoEleccionService
     {
+        private List<ResultadosElección> resultados;
+        public ResultadoEleccionService() 
+        {
+            resultados = new List<ResultadosElección>();
+
+        }
+
+        public List<ResultadosElección> ObtenerResultadosPorEleccion(int IdEleccion)
+        {
+            return resultados.Where(r=> r.IdEleccion == IdEleccion).ToList();
+        }
     }
 }
