08ea3fd582d77a5a02719de8034680cefec7960f
isatkm21 <isabellamanjarres67@gmail.com>
2023-11-10 01:03:22 -0500

    septimo

--- DAL/Repositorio.cs
+++ DAL/Repositorio.cs
@@ -160,7 +160,161 @@ namespace DAL
                         while ((line = reader.ReadLine()) != null)
                         {
                             string[] campos = line.Split(',');
+                            if (campos.Length == 3) 
+                            {
+                                int Id = int.Parse(campos[0]);
+                                string Nombre = campos[1];
+                                string PartidoPolitico = campos[2];
+
+                                Candidato Candidato = new Candidato
+                                {
+                                    Id = Id,
+                                    Nombre = Nombre,
+                                    PartidoPolitico = PartidoPolitico
+                                };
+                                candidatos.Add(Candidato);
+                            }
                         }
+                    }
+                }
+                return candidatos;
+            }
+
+            public void Guardar(Candidato candidato)
+            {
+                if (!ObtenerTodos().Any( c => c.Nombre == candidato.Nombre)) 
+                {
+                    using (StreamWriter writer = new StreamWriter(candidatosFilePath, true))
+                    {
+                        string line = $"{candidato.Id}, {candidato.Nombre}, {candidato.PartidoPolitico}";
+                        writer.WriteLine(line);
+                    }
+                    //string line = $"{candidato.Id}, {candidato.Nombre}, {candidato.PartidoPolitico}";
+                }
+                else
+                {
+                    throw new Exception("Este candidato ya ha sido registrado");
+                }
+            }
+        }
+
+        public interface IVotoRepository
+        {
+            Voto ObtenerPorId(int id);
+            List<Voto> ObtenerTodos();
+            void Guardar(Voto voto);
+        }
+        public class VotoRepository:IVotoRepository
+        {
+            private string votosFilePath = "Votos.txt";
+
+            public Voto ObtenerPorId(int id)
+            {
+                return ObtenerTodos().FirstOrDefault(v => v.Id == id);
+            }
+            public List<Voto> ObtenerTodos()
+            {
+                List <Voto> votos = new List <Voto>();
+                if (File.Exists(votosFilePath)) 
+                {
+                    using (StreamReader reader = new StreamReader(votosFilePath))
+                    {
+                        string line;
+                        while((line = reader.ReadLine()) != null)
+                        {
+                            string[] campos = line.Split(',');
+                            if (campos.Length == 4) 
+                            {
+                                int Id = int.Parse(campos[0]);
+                                int IdUsuario = int.Parse(campos[1]);
+                                int IdEleccion = int.Parse(campos[2]);
+                                int IdCandidato = int.Parse(campos[3]);
+
+
+                                Voto voto = new Voto
+                                {
+                                    Id = Id,
+                                    IdUsuario = IdUsuario,
+                                    IdEleccion = IdEleccion,
+                                    IdCandidato = IdCandidato,
+                                };
+                                votos.Add(voto);
+                            }
+                        }
+                    }
+                }
+                return votos;
+            }
+            public void Guardar (Voto voto)
+            {
+                if (!ObtenerTodos().Any(v=> v.IdUsuario == voto.IdUsuario && v.IdEleccion == voto.IdEleccion)) 
+                {
+                    using (StreamWriter writer = new StreamWriter(votosFilePath, true))
+                    {
+                        string line = $"{voto.Id}, {voto.IdUsuario}, {voto.IdCandidato}";
+                        writer.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Voto duplicado para el mismo usuario y elección");
+                }
+            }
+        }
+
+        public interface IResultadosEleccionRepository
+        {
+            List<ResultadosElección> ObtenerResultadosPorEleccion(int idEeccion);
+            void GuardarResultados(List<ResultadosElección> resultados);
+        }
+
+        public class ResultadosEleccionRepository : IResultadosEleccionRepository 
+        {
+            private string resultadosFilePath = "Resultados.txt";
+
+            public List<ResultadosElección> ObtenerResultadosPorEleccion(int idEleccion)
+            {
+                return ObtenerTodos().Where(r => r.IdEleccion ==  idEleccion).ToList();
+            }
+            public List<ResultadosElección> ObtenerTodos()
+            {
+                List<ResultadosElección> resultados = new List<ResultadosElección>();
+                if (File.Exists(resultadosFilePath))
+                {
+                    using (StreamReader reader = new StreamReader(resultadosFilePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] campos = line.Split(' ');
+                            if (campos.Length == 3)
+                            {
+                                int IdEleccion = int.Parse(campos[0]);
+                                int IdCandidato = int.Parse(campos[1]);
+                                int VotosObtenidos = int.Parse(campos[2]);
+
+                                ResultadosElección resultado = new ResultadosElección
+                                {
+                                    IdEleccion = IdEleccion,
+                                    IdCandidato = IdCandidato,
+                                    VotosObtenidos = VotosObtenidos,
+
+                                };
+                                resultados.Add(resultado);
+                            }
+                        }
+                    }
+                }
+                return resultados;
+            }
+
+            public void GuardarResultados(List<ResultadosElección> resultados)
+            {
+                using (StreamWriter writer = new StreamWriter(resultadosFilePath)) 
+                {
+                    foreach (var resultado in resultados)
+                    {
+                        writer.WriteLine($"{resultado.IdEleccion}, {resultado.IdCandidato}, {resultado.VotosObtenidos}");
                     }
                 }
             }
