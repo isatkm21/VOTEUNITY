009405c773d9471afc3a3c4584a2d8a29da009fa
isatkm21 <isabellamanjarres67@gmail.com>
2023-11-11 23:51:00 -0500

    Update

--- GUI/InfoCandidato2.Designer.cs
+++ GUI/InfoCandidato2.Designer.cs
@@ -34,6 +34,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -47,12 +49,14 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.75773F));
             this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox3, 1, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 88);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 64.94253F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35.05747F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 350);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.06443F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.93557F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 359);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
@@ -82,7 +86,7 @@
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(5, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(280, 217);
+            this.pictureBox2.Size = new System.Drawing.Size(280, 209);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
@@ -94,11 +98,35 @@
             this.richTextBox1.ForeColor = System.Drawing.Color.Transparent;
             this.richTextBox1.Location = new System.Drawing.Point(293, 5);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(478, 217);
+            this.richTextBox1.Size = new System.Drawing.Size(478, 209);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "        Principales propuestas\n \n- Reducción del desempleo\n- Reestructuración al " +
     "sistema pensional\n- Mejora al sistema de salud";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox2.Font = new System.Drawing.Font("Nirmala UI", 9.25F, System.Drawing.FontStyle.Bold);
+            this.richTextBox2.ForeColor = System.Drawing.Color.Transparent;
+            this.richTextBox2.Location = new System.Drawing.Point(5, 222);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(280, 132);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "       Datos Personales        \n- Nombre: Ingrid Betancourt\n\n- Edad: 60 años\n\n- P" +
+    "rofesión: Politóloga\n\n- Universidad: Instituto de Estudios Políticos de      Par" +
+    "ís";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox3.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold);
+            this.richTextBox3.ForeColor = System.Drawing.Color.Transparent;
+            this.richTextBox3.Location = new System.Drawing.Point(293, 222);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(478, 132);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
             // 
             // InfoCandidato2
             // 
@@ -126,5 +154,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
     }
 }
